d2ddbcf563796a4c653fe43441851fc769b5c8ad
b07080 <tom@turbotektuning.com>
2013-06-26 23:09:16 -0400

    just added some code, put in a function that I can hopefully use later

--- Delawder_Thomas_assignment4.js
+++ Delawder_Thomas_assignment4.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+
+//First line of code for library
+// you can call it and add any food into it
+
+var foodDemand = function(food) {
+    console.log("I want to eat" + " " + food);
+    
+   
+};
+
+foodDemand("dog meat");
+
+
+
+
