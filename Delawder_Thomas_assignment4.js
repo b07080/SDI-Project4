a42eea98f7ff12cf8ce47aa0c5ceb4326ffa781f
b07080 <tom@turbotektuning.com>
2013-06-27 10:32:41 -0400

    trying to input function knowledge and parse into something for the p4 assignment

--- Delawder_Thomas_assignment4.js
+++ Delawder_Thomas_assignment4.js
@@ -26,3 +26,30 @@ orangeCost(5);
 var areaBox = function(length, width) {
          return length * width;
 };
+
+
+
+//____________________________________________________________
+// global variables
+
+// functions
+var myFunction = function(arg1, arg2) {
+    // local variables
+    var booleanReturn;
+    
+    if (arg1 < arg2) {
+        console.log("true");
+        booleanReturn = true;
+    } else {
+        console.log("false");
+        booleanReturn = false;
+    }
+    
+    return booleanReturn;
+    
+}
+
+//main code
+console.log(booleanReturn);
+
+//____________________________
